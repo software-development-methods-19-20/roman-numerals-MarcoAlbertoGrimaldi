5d6ff3f009aa38797b001af270b6bcdfe6ecc412
MarcoAlbertoGrimaldi <marco.alberto.grimaldi@gmail.com>
2019-11-12 09:44:44 +0100

    1 to I

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,8 +1,8 @@
 package test.roman;
 
 import dssc.roman.RomanNumeral;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DecimalToRomanNumeralTest {
@@ -10,5 +10,12 @@ public class DecimalToRomanNumeralTest {
     void zeroIsEmptyString() {
         RomanNumeral romanNumeral = new RomanNumeral(0);
         assertEquals("", romanNumeral.toString());
+    }
+
+    @Test
+    //@Disabled
+    void oneIsI() {
+        RomanNumeral romanNumeral = new RomanNumeral(1);
+        assertEquals("I", romanNumeral.toString());
     }
 }
